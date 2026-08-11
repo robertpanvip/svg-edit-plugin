a94fa6888d5b4e5ca0506d15323fc80a5f384de8
robertpanvip <robertpanvip@163.com>
2026-08-11 23:04:40 +0800

    feat: SvgEditorProvider registered order=last to coexist with built-in image viewer

---
+++ plugin/src/main/kotlin/com/example/svgeditor/plugin/SvgEditorProvider.kt
@@ -0,0 +1,31 @@
+package com.example.svgeditor.plugin
+
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.fileEditor.FileEditorPolicy
+import com.intellij.openapi.fileEditor.FileEditorProvider
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+
+/**
+ * Makes [SvgPreviewEditor] available for `.svg` files. Registered with `order="last"` so the
+ * built-in IDEA SVG image viewer stays the default tab on open, while SvgEasy coexists as an
+ * additional tab (it is not forced to replace the image viewer).
+ *
+ * To make SvgEasy the *default* `.svg` editor instead (replacing the image viewer), change the
+ * registration to `order="first"`.
+ */
+class SvgEditorProvider : FileEditorProvider {
+    override fun accept(
+        project: Project,
+        file: VirtualFile,
+    ): Boolean = file.extension.equals("svg", ignoreCase = true)
+
+    override fun createEditor(
+        project: Project,
+        file: VirtualFile,
+    ): FileEditor = SvgPreviewEditor(project, file)
+
+    override fun getEditorTypeId(): String = "SvgEasy.text.editor"
+
+    override fun getPolicy(): FileEditorPolicy = FileEditorPolicy.HIDE_DEFAULT_EDITOR
+}
